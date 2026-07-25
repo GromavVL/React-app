90fae5be40e8e654fbdf565c42365a1bd3c8b06a
Vlad <vlad086367@gmail.com>
2026-07-25 13:51:16 +0300

    -

---
+++ src/components/EventsForm/index.jsx
@@ -0,0 +1,12 @@
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+
+function EventsForm () {
+  return <section>
+    <span>Форма заповлення для івентів</span>
+    <Formik>
+      
+    </Formik>
+  </section>;
+}
+
+export default EventsForm;
---
+++ src/pages/EventsPage/index.jsx
@@ -0,0 +1,13 @@
+import EventsForm from '../../components/EventsForm';
+import EventsList from '../../components/EventsList';
+
+function EventsPage () {
+  return (
+    <>
+      <EventsForm />
+      <EventsList />
+    </>
+  );
+}
+
+export default EventsPage;
